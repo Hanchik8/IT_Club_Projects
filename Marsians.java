9bedd6fc391cde73568ac78966396a13989700bc
Nurlanov <nurlanovazizhan@gmail.com>
2024-09-28 11:57:29 +0600

    added a method, which one create different random numbers

--- Marsians.java
+++ Marsians.java
@@ -1,27 +1,44 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Marsians {
+     Scanner scanner = new Scanner(System.in);
    public static void main(String[] args) {
-      int randomPosisionOfBoxes= (int) Math.round(1 + (Math.random() * (7-1))); 
-      int randomPosisionOfBoxes1 = (int) Math.round(1 + (Math.random() * (7-1))); 
-      int randomPosisionOfBoxes2 = (int) Math.round(1 + (Math.random() * (7-1)));
+      Marsians obj = new Marsians();
+      //Creates random box positions
+      similary_check();
+      int Checking_for_guessing_position = obj.scanner.nextInt();
+      int Checking_for_guessing_position1 = obj.scanner.nextInt();
+      int Checking_for_guessing_position2 = obj.scanner.nextInt();
+      
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-      System.out.println(randomPosisionOfBoxes);
-      System.out.println(randomPosisionOfBoxes1);
-      System.out.println(randomPosisionOfBoxes2);
+   }
+   public static void similary_check() {
+      Random rand = new Random();
+      int randomPosisionOfBoxes = 1 + rand.nextInt(6); 
+      int randomPosisionOfBoxes1 = 1 + rand.nextInt(6); 
+      int randomPosisionOfBoxes2 = 1 + rand.nextInt(6);
+   
+      while (randomPosisionOfBoxes == randomPosisionOfBoxes1 || 
+             randomPosisionOfBoxes2 == randomPosisionOfBoxes1 || 
+             randomPosisionOfBoxes == randomPosisionOfBoxes2) {
+   
+          if (randomPosisionOfBoxes == randomPosisionOfBoxes1) {
+              randomPosisionOfBoxes1 = 1 + rand.nextInt(6);
+          }
+   
+          if (randomPosisionOfBoxes2 == randomPosisionOfBoxes1 || 
+          randomPosisionOfBoxes2 == randomPosisionOfBoxes) {
+              randomPosisionOfBoxes2 = 1 + rand.nextInt(6);
+          }
+   
+          if (randomPosisionOfBoxes == randomPosisionOfBoxes2) {
+              randomPosisionOfBoxes = 1 + rand.nextInt(6);
+          }
+      }
    }
 }
+

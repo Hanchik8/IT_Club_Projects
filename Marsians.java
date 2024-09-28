27e18b243e1ac308cb4ee12380186d09f5e5f51b
Nurlanov <nurlanovazizhan@gmail.com>
2024-09-28 10:46:46 +0600

    Start of the project, added random position

---
+++ Marsians.java
@@ -0,0 +1,27 @@
+public class Marsians {
+   public static void main(String[] args) {
+      int randomPosisionOfBoxes= (int) Math.round(1 + (Math.random() * (7-1))); 
+      int randomPosisionOfBoxes1 = (int) Math.round(1 + (Math.random() * (7-1))); 
+      int randomPosisionOfBoxes2 = (int) Math.round(1 + (Math.random() * (7-1)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      System.out.println(randomPosisionOfBoxes);
+      System.out.println(randomPosisionOfBoxes1);
+      System.out.println(randomPosisionOfBoxes2);
+   }
+}

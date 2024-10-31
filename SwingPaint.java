4fd574950a46ac79d7188dce77cb8334d8a90995
Nurlanov <nurlanovazizhan@gmail.com>
2024-10-31 10:57:03 +0600

    added a save system in the png format

--- SwingPaint.java
+++ SwingPaint.java
@@ -1,86 +1,100 @@
 
-// import java.awt.BorderLayout;
-// import java.awt.Container;
-// import java.awt.event.ActionEvent;
-// import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-// import javax.swing.JButton;
-// import javax.swing.JFrame;
-// import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-// public class SwingPaint {
+public class SwingPaint {
 
-//    JButton clearBtn, blackBtn, blueBtn, greenBtn, redBtn, magentaBtn;
-//    DrawArea drawArea;
-//    ActionListener actionListener = new ActionListener() {
+   JButton clearBtn, blackBtn, blueBtn, greenBtn, redBtn, pinkBtn, saveBtn;
+   DrawArea drawArea;
 
-//       public void actionPerformed(ActionEvent e) {
-//          if (e.getSource() == clearBtn) {
-//             drawArea.clear();
-//          } else if (e.getSource() == blackBtn) {
-//             drawArea.black();
-//          } else if (e.getSource() == blueBtn) {
-//             drawArea.blue();
-//          } else if (e.getSource() == greenBtn) {
-//             drawArea.green();
-//          } else if (e.getSource() == redBtn) {
-//             drawArea.red();
-//          } else if (e.getSource() == magentaBtn) {
-//             drawArea.magenta();
-//          }
-//       }
-//    };
+   ActionListener actionListener = new ActionListener() {
 
-//    public static void main(String[] args) {
-//       new SwingPaint().show();
-//    }
+      public void actionPerformed(ActionEvent e) {
+         switch (e.getActionCommand()) {
+            case "Clear":
+               drawArea.clear();
+               break;
+            case "Save":
+               drawArea.clear();
+               break;
+            case "Black":
+               drawArea.black();
+               break;
+            case "Blue":
+               drawArea.blue();
+               break;
+            case "Green":
+               drawArea.green();
+               break;
+            case "Red":
+               drawArea.red();
+               break;
+            case "Pink":
+               drawArea.pink();
+               break;
+            default:
+               System.out.println("Unknown action");
+         }
+      }
+   };
 
-//    public void show() {
-//       // create main frame
-//       JFrame frame = new JFrame("Swing Paint");
-//       Container content = frame.getContentPane();
-//       // set layout on content pane
-//       content.setLayout(new BorderLayout());
-//       // create draw area
-//       drawArea = new DrawArea();
+   public static void main(String[] args) {
+      new SwingPaint().show();
+   }
 
-//       // add to content pane
-//       content.add(drawArea, BorderLayout.CENTER);
+   public void show() {
+      // create main frame
+      JFrame frame = new JFrame("Swing Paint");
+      Container content = frame.getContentPane();
+      // set layout on content pane
+      content.setLayout(new BorderLayout());
+      // create draw area
+      drawArea = new DrawArea();
 
-//       // create controls to apply colors and call clear feature
-//       JPanel controls = new JPanel();
+      // add to content pane
+      content.add(drawArea, BorderLayout.CENTER);
 
-//       clearBtn = new JButton("Clear");
-//       clearBtn.addActionListener(actionListener);
-//       blackBtn = new JButton("Black");
-//       blackBtn.addActionListener(actionListener);
-//       blueBtn = new JButton("Blue");
-//       blueBtn.addActionListener(actionListener);
-//       greenBtn = new JButton("Green");
-//       greenBtn.addActionListener(actionListener);
-//       redBtn = new JButton("Red");
-//       redBtn.addActionListener(actionListener);
-//       magentaBtn = new JButton("Magenta");
-//       magentaBtn.addActionListener(actionListener);
+      // create controls to apply colors and call clear feature
+      JPanel controls = new JPanel();
 
-//       // add to panel
-//       controls.add(greenBtn);
-//       controls.add(blueBtn);
-//       controls.add(blackBtn);
-//       controls.add(redBtn);
-//       controls.add(magentaBtn);
-//       controls.add(clearBtn);
+      clearBtn = new JButton("Clear");
+      clearBtn.addActionListener(actionListener);
+      JButton saveBtn = new JButton("Save");
+      saveBtn.addActionListener(e -> drawArea.saveImage()); // Calls saveImage() in DrawArea
+      frame.add(saveBtn, BorderLayout.SOUTH);
+      blackBtn = new JButton("Black");
+      blackBtn.addActionListener(actionListener);
+      blueBtn = new JButton("Blue");
+      blueBtn.addActionListener(actionListener);
+      greenBtn = new JButton("Green");
+      greenBtn.addActionListener(actionListener);
+      redBtn = new JButton("Red");
+      redBtn.addActionListener(actionListener);
+      pinkBtn = new JButton("Pink");
+      pinkBtn.addActionListener(actionListener);
 
-//       // add to content pane
-//       content.add(controls, BorderLayout.NORTH);
+      // add to panel
+      controls.add(greenBtn);
+      controls.add(blueBtn);
+      controls.add(blackBtn);
+      controls.add(redBtn);
+      controls.add(pinkBtn);
+      controls.add(clearBtn);
 
-//       frame.setSize(600, 600);
-//       // can close frame
-//       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//       // show the swing paint result
-//       frame.setVisible(true);
+      // add to content pane
+      content.add(controls, BorderLayout.NORTH);
 
-//       // Now you can try our Swing Paint !!! Enjoy :D
-//    }
+      frame.setSize(600, 600);
+      Toolkit toolkit = Toolkit.getDefaultToolkit();
+      Dimension dimention = toolkit.getScreenSize();
+      frame.setBounds(dimention.width / 2 - 400, dimention.height / 2 - 300, 800, 550);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setVisible(true);
+   }
 
-// }
+}
